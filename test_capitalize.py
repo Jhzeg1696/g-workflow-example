566b63dd503a8f5cfd86cb0fae4d7edcade8b569
Jhzeg1696 <83097292+Jhzeg1696@users.noreply.github.com>
2022-03-03 14:05:01 -0600

    First commit

---
+++ test_capitalize.py
@@ -0,0 +1,8 @@
+def capitalize_string(s):
+    if not isinstance(s, str):
+        raise TypeError('Provide a string')
+    
+    return s.capitalize()
+
+def test_capitalize_String():
+    assert capitalize_string('test') == 'Test'
